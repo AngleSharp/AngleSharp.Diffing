0c53577d9ea2450a9402c7a6e7e9db9e18311538
Egil Hansen <egil@assimilated.dk>
2021-04-22 12:25:16 +0000

    signing both test and src project to allow internal visiblity from test to src, upgraded dependencies and target framework of test project

--- src/AngleSharp.Diffing.Tests/ShouldlyTestExtensions.cs
+++ src/AngleSharp.Diffing.Tests/ShouldlyTestExtensions.cs
@@ -7,19 +7,7 @@ using Shouldly;
 namespace AngleSharp.Diffing
 {
     public static class ShouldlyTestExtensions
-    {
-        // The ShouldSatisfyAllConditions are here until https://github.com/shouldly/shouldly/issues/472 lands in a release of Shouldly.
-        public static void ShouldSatisfyAllConditions<T>(this T actual, params Action<T>[] conditions) => ShouldSatisfyAllConditions(actual, () => null, conditions);
-        public static void ShouldSatisfyAllConditions<T>(this T actual, string customMessage, params Action<T>[] conditions) => ShouldSatisfyAllConditions(actual, () => customMessage, conditions);
-        public static void ShouldSatisfyAllConditions<T>(this T actual, Func<string?> customMessage, params Action<T>[] conditions)
-        {
-            var convertedConditions = conditions
-                            .Select(a => new Action(() => a(actual)))
-                            .ToArray();
-
-            actual.ShouldSatisfyAllConditions(customMessage, convertedConditions);
-        }
-
+    {        
         public static void ShouldAllBe<T>(this IEnumerable<T> actual, Func<T, int, bool> elementPredicate)
         {
             actual.Select(elementPredicate).ShouldAllBe(x => x);
