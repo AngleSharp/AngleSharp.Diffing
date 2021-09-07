067647276ba63192e3bbc68abd47bf96fba319d7
Rishat Gildanov <grishat@gmail.com>
2021-09-07 09:54:19 +0500

    Fix and simplify IgnoreChildrenElementComparerTest

--- src/AngleSharp.Diffing.Tests/Strategies/ElementStrategies/IgnoreChildrenElementComparerTest.cs
+++ src/AngleSharp.Diffing.Tests/Strategies/ElementStrategies/IgnoreChildrenElementComparerTest.cs
@@ -28,7 +28,7 @@ namespace AngleSharp.Diffing.Strategies.ElementStrategies
             IgnoreChildrenElementComparer.Compare(comparison, CompareResult.Skip).ShouldBe(CompareResult.Skip);
         }
 
-        [Theory(DisplayName = "When a control element has 'diff:ignoreChildren' attribute, SameAndBreak is returned")]
+        [Theory(DisplayName = "When a control element has 'diff:ignoreChildren' attribute, CompareResult.SkipChildren flag is returned")]
         [InlineData(@"<p diff:ignoreChildren></p>")]
         [InlineData(@"<p diff:ignoreChildren=""true""></p>")]
         [InlineData(@"<p diff:ignoreChildren=""TRUE""></p>")]
@@ -41,29 +41,13 @@ namespace AngleSharp.Diffing.Strategies.ElementStrategies
             IgnoreChildrenElementComparer.Compare(comparison, CompareResult.Different).ShouldBe(CompareResult.Different | CompareResult.SkipChildren);
         }
 
-        [Fact(DisplayName = "When a control element has 'diff:ignoreChildren', calling Build() with DefaultOptions() returns expected diffs")]
-        public void Test003()
-        {
-            var control = @"<p attr=""foo"" missing diff:ignoreChildren>hello <em>world</em></p>";
-            var test    = @"<p attr=""bar"" unexpected>world says <strong>hello</strong></p>";
-
-            var diffs = DiffBuilder
-                .Compare(control)
-                .WithTest(test)
-                .Build()
-                .ToList();
-
-            diffs.Count.ShouldBe(3);
-            diffs.SingleOrDefault(x => x is AttrDiff).ShouldNotBeNull();
-            diffs.SingleOrDefault(x => x is MissingAttrDiff).ShouldNotBeNull();
-            diffs.SingleOrDefault(x => x is UnexpectedAttrDiff).ShouldNotBeNull();
-        }
-
         [Theory(DisplayName = "When a control element has 'diff:ignoreChildren', calling Build() with DefaultOptions() returns empty diffs")]
-        [InlineData(@"<p attr=""foo"" diff:ignoreChildren>hello <em>world</em></p>",
-                        @"<p attr=""foo"">world says <strong>hello</strong></p>")]
-        [InlineData(@"<p attr=""foo"" expected diff:ignoreChildren>hello <em>world</em></p>",
-                        @"<p attr=""foo"" expected>world says <strong>hello</strong></p>")]
+        [InlineData(@"<p diff:ignoreChildren>hello <em>world</em></p>",
+                         @"<p>world says <strong>hello</strong></p>")]
+        [InlineData(@"<p diff:ignoreChildren>hello</p>",
+                         @"<p>world says <strong>hello</strong></p>")]
+        [InlineData(@"<p diff:ignoreChildren>hello <em>world</em></p>",
+                         @"<p>world says</p>")]
         public void Test004(string control, string test)
         {
             var diffs = DiffBuilder
