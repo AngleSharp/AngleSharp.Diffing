b271048de418b73f8975090824f5e12d74aff1a3
Sebastian Stehle <sebastian@squidex.io>
2023-03-22 11:43:01 +0100

    Update src/AngleSharp.Diffing/Core/AttributeComparison.cs
    Co-authored-by: Egil Hansen <egil@assimilated.dk>

--- src/AngleSharp.Diffing/Core/AttributeComparison.cs
+++ src/AngleSharp.Diffing/Core/AttributeComparison.cs
@@ -3,6 +3,8 @@ namespace AngleSharp.Diffing.Core;
 /// <summary>
 /// A match between two attributes that should be compared.
 /// </summary>
+/// <param name="Control">Gets the control attribute which the <see cref="Test"/> attribute is supposed to match.</param>
+/// <param name="Test">Gets the test attribute which should be compared to the <see cref="Control"/> attribute.</param>
 public readonly record struct AttributeComparison(in AttributeComparisonSource Control, in AttributeComparisonSource Test)
 {
     /// <summary>
