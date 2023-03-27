c2039f98e12a5b9d068e42cccaff629d9bace315
Egil Hansen <egil@assimilated.dk>
2023-03-27 15:16:40 +0000

    Move types into two files

--- src/AngleSharp.Diffing/Core/CompareResult.cs
+++ src/AngleSharp.Diffing/Core/CompareResult.cs
@@ -49,36 +49,3 @@ public readonly record struct CompareResult(CompareDecision Decision, IDiff? Dif
     /// </summary>
     public bool IsSameOrSkip() => this == Same || this == Skip;
 }
-
-/// <summary>
-/// Represents the decision of a comparison.
-/// </summary>
-[Flags]
-public enum CompareDecision
-{
-    /// <summary>
-    /// Use when the compare result is unknown.
-    /// </summary>
-    Unknown = 0,
-    /// <summary>
-    /// Use when the two compared nodes or attributes are the same.
-    /// </summary>
-    Same = 1,
-    /// <summary>
-    /// Use when the two compared nodes or attributes are the different.
-    /// </summary>
-    Different = 2,
-    /// <summary>
-    /// Use when the comparison should be skipped and any child-nodes or attributes skipped as well.
-    /// </summary>
-    Skip = 4,
-    /// <summary>
-    /// Use when the comparison should skip any child-nodes.
-    /// </summary>
-    SkipChildren = 8,
-    /// <summary>
-    /// Use when the comparison should skip any attributes.
-    /// </summary>
-    SkipAttributes = 16,
-}
-
